137f7bedae2f731150276ebd4c47362371498464
XavierJDN <jouslin@nowray.net>
2021-09-10 17:46:57 -0400

    finish code for Point2D and PointOperator but not test

--- src/tp1/Point2d.java
+++ src/tp1/Point2d.java
@@ -1,4 +1,5 @@
 package tp1;
+import java.lang.*;
 
 public class Point2d extends AbstractPoint {
     private final Integer X = 0;
@@ -6,12 +7,12 @@ public class Point2d extends AbstractPoint {
 
     // TODO creer un point en 2d avec 2 donnees
     public Point2d(Double x, Double y) {
-        super(null /*...*/);
+        super(new Double[]{x,y});
     }
 
     // TODO creer un point a partir d'un vecteur de donnees
     public Point2d(Double[] vector) {
-        super(null /*...*/);
+        super(vector);
     }
 
     public Double X() { return vector[X];}
@@ -20,46 +21,54 @@ public class Point2d extends AbstractPoint {
     // TODO appliquer la transalation sur la classe courrante et retourner la reference
     @Override
     public Point2d translate(Double[] translateVector) {
-        return null;
+        tp1.PointOperator.translate(this.vector, translateVector);
+        return this;
     }
 
     // TODO appliquer la transalation sur la classe courrante et retourner la reference
     public Point2d translate(Point2d translateVector) {
-        return null;
+        tp1.PointOperator.translate(this.vector, translateVector.vector);
+        return this;
     }
 
     // TODO appliquer la rotation sur la classe courrante et retourner la reference
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        return null;
+        tp1.PointOperator.rotate(this.vector, rotationMatrix);
+        return this;
     }
 
     // TODO appliquer la rotation sur la classe courrante et retourner la reference
     public Point2d rotate(Double angle) {
-        return null;
+        tp1.PointOperator.rotate(this.vector, new Double[][]{{java.lang.Math.cos(angle), -java.lang.Math.sin(angle) },
+                                                            {java.lang.Math.sin(angle), java.lang.Math.cos(angle)   }});
+        return this;
     }
 
     // TODO appliquer la division sur la classe courrante et retourner la reference
     @Override
     public Point2d divide(Double divider) {
-        return null;
+        tp1.PointOperator.divide(this.vector, divider);
+        return this;
     }
 
     // TODO appliquer la multiplication sur la classe courrante et retourner la reference
     @Override
     public Point2d multiply(Double multiplier) {
-        return null;
+        tp1.PointOperator.multiply(this.vector, multiplier);
+        return this;
     }
 
     // TODO appliquer la addition sur la classe courrante et retourner la reference
     @Override
     public Point2d add(Double adder) {
-        return null;
+        tp1.PointOperator.add(this.vector, adder);
+        return this;
     }
 
     // TODO creer un nouveau point.
     @Override
     public Point2d clone() {
-        return this;
+        return new Point2d(this.X(), this.Y());
     }
 }
--- src/tp1/PointOperator.java
+++ src/tp1/PointOperator.java
@@ -6,26 +6,44 @@ public final class PointOperator {
 
     // TODO appliquer la translation sur le vecteur d'entree.
     public static void translate(Double[] vector, Double[] translateVector) {
-        // ...
+        for (int i=0; i < vector.length; i++)
+            vector[i] += translateVector[i];
     }
 
     // TODO appliquer la translation sur le vecteur d'entree.
     public static void rotate(Double[] vector, Double[][] rotationMatrix) {
-        //
+        for (int i=0; i< rotationMatrix.length; i++){
+            Double[] coeff_vector = new Double[]{};
+             for (int j=0; j < rotationMatrix[i].length; j++){
+                 coeff_vector[j] = rotationMatrix[i][j] * vector[j];
+             }
+             //addition
+            Double result=0.0;
+             for (Double vector_result: coeff_vector){
+                result += vector_result;
+            }
+            vector[i] = result;
+        }
     }
 
     // TODO appliquer la translation sur le vecteur d'entree.
     public static void divide(Double[] vector, Double divider) {
-        // ...
+        for(Double vec:vector){
+            vec/=divider;
+        }
     }
 
     // TODO appliquer la translation sur le vecteur d'entree.
     public static void multiply(Double[] vector, Double multiplier) {
-        // ...
+        for (Double vec:vector){
+            vec*=multiplier;
+        }
     }
 
     // TODO appliquer la translation sur le vecteur d'entree.
     public static void add(Double[] vector, Double adder) {
-        // ...
+        for (Double vec:vector){
+            vec+=adder;
+        }
     }
 }
